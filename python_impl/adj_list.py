9dbc32a942a49d62fa66b52d7668bee537c72f07
Jordan Irgang <jordanirgang@gmail.com>
2024-12-27 17:19:51 -0500

    updated python xml parser anr and graph

--- python_impl/adj_list.py
+++ python_impl/adj_list.py
@@ -1,5 +1,5 @@
 import sys
-
+import xml.etree.ElementTree as ET
 class Node:
     def __init__(self,data):
         self.data = data
@@ -8,17 +8,44 @@ class Node:
 class Graph:
     def __init__(self):
         self.adj_list = []
+        self.look_up = {}
+        self.node_count = 0
+
     def addNode(self,data):
-        self.adj_list.append(Node(data))
+        if data not in self.look_up:
+            self.adj_list.append(Node(data))
+            self.look_up[data]= self.node_count
+            self.node_count +=1
 
     def addEdge(self,src:int,dest:int):
         self.adj_list[src].children.append(self.adj_list[dest])
+
+    def addEdgeByName(self,src:str,dest:str):
+        print(("list",self.adj_list[0]))
+        src_int = self.look_up[src]
+        dest_int = self.look_up[dest]
+        self.addEdge(src_int,dest_int)
+
     def printAdjacencyList(self):
         for i in self.adj_list:
-            sys.stdout.write(i.data)
+            sys.stdout.write(str(i.data)+"->")
             for j in i.children:
-                sys.stdout.write("->" + str(j.data)+"\n")
+                sys.stdout.write(str(j.data)+"&")
             sys.stdout.write("\n")
+
+def urdf2graph(path:str):
+    graph=Graph()
+    tree = ET.parse(path)
+    root = tree.getroot()
+    for link in root.findall('./link'):
+        graph.addNode(link.attrib['name'])
+    for joint in root.findall('./joint'):
+        data = (joint.find('parent').attrib['link'],
+                joint.find('child').attrib['link'],
+                joint.find('origin').attrib['xyz'])
+        graph.addEdgeByName(data[0],data[1])
+    return graph
+    
 
 
 
@@ -27,4 +54,20 @@ if __name__ == "__main__":
     graph.addNode("A")
     graph.addNode("B")
     graph.addEdge(0,1)
+    graph.addEdgeByName("A","B")
     graph.printAdjacencyList()
+    tree = ET.parse('robot_test.xml')
+    root = tree.getroot()
+    print(root.findall('./link'))
+    for link in root.findall('./link'):
+        print(link.attrib['name'])
+    for joint in root.findall('./joint'):
+        data = (joint.find('parent').attrib['link'],
+                joint.find('child').attrib['link'],
+                joint.find('origin').attrib['xyz'])
+        print(data)
+    newGraph= urdf2graph('robot_test.xml')
+    print(newGraph)
+    newGraph.printAdjacencyList()
+
+
