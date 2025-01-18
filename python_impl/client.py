a5b75bede98dba24bb3f0fe518acbb8745ca4b5a
Jordan Irgang <jordanirgang@gmail.com>
2025-01-18 12:04:57 -0800

    syncing server progress

--- python_impl/client.py
+++ python_impl/client.py
@@ -1,15 +1,30 @@
 #place on pupet
 import socket
 import yamlLoader as yl
+import byteStreamHandler as BSH
+
+class Client:
+
+    def __init__(self,server_ip="127.0.0.1",server_port=8080,client_port= 8888):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_info = (server_ip,server_port)
+        #subscribe
+        isNotConnected=True
+        while(isNotConnected):
+            self.client_socket.sendto(bytes(client_port),self.server_info)
+
+        self.byte_decoder = BSH.ByteStreamHandler()
+
+    def send_int_array(self,data_to_send):
+        msg2send = self.byte_encoder.compose_byte_frame(data_to_send)
+        self.client_socket.sendto(msg2send, )
 
 
 if __name__ == "__main__":
-    msg = 5 
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ls = {2,3,4,5}
-    #msg2send = msg.to_bytes(ls, 'big')
-    msg2send = bytearray(ls)
-    print(msg2send.decode("utf-8"),"sent")
-    client_socket.sendto(msg2send, (yl.dictionary['server']['ip'],yl.dictionary['server']['port']))
+    
+   
+    #(yl.dictionary['server']['ip'],yl.dictionary['server']['port'])
+    print("sent")
+    
 
     
--- python_impl/server.py
+++ python_impl/server.py
@@ -1,15 +1,32 @@
 import socket
 import yaml
 import yamlLoader as yl
+import byteStreamHandler as BSH
+
+class Server:
+    subscriber_addresses = []
+    def __init__(self,server_ip="",server_port=8080):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_info = (server_ip,server_port)
+        self.server_socket.bind('',self.server_info)
+    
+    def get_subscribers(self):
+        port, address = self.server_socket.recvfrom(1024)
+        print(port)
+        self.subscriber_addresses.append((address[0],int.from_bytes(port)))
+
+        self.server_socket.send(bytes('ok'),self.subscriber_addresses[0])
+
 
 
 if __name__ == "__main__":
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('', yl.dictionary['server']['port']))
+    ('', yl.dictionary['server']['port']))
+    byte_encoder = BSH.ByteStreamHandler()
 
-    message, address = server_socket.recvfrom(1024)
-    msg = list(message)
+    data_to_send = [0,134,247,39,40,52]
+
+    msg = byte_decoder.decompose_byte_frame(message)
     print((msg,address))
 
 
