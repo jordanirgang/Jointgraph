aa9456c8553177dd4dad75354d19a0dcbb1ec649
Jordan Irgang <jordanirgang@gmail.com>
2025-01-18 22:14:28 -0500

    client makes intial connection

--- python_impl/client.py
+++ python_impl/client.py
@@ -7,23 +7,36 @@ class Client:
 
     def __init__(self,server_ip="127.0.0.1",server_port=8080,client_port= 8888):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server_info = (server_ip,server_port)
+        self.client_port = client_port
+        self.client_socket.bind(('',self.client_port))
+
+    def subscribe(self,server_ip= "127.0.0.1",server_port=8080):
         #subscribe
+        server_info = (server_ip,server_port)
+        self.client_socket.settimeout(10)
         isNotConnected=True
         while(isNotConnected):
-            self.client_socket.sendto(bytes(client_port),self.server_info)
+            self.client_socket.sendto(self.client_port.to_bytes(2,'little'),server_info)
+            message,address = self.client_socket.recvfrom(1024)
+            response_code = int.from_bytes(message,'little') 
+            isNotConnected = response_code != 200
+            print(response_code==response_code)
 
         self.byte_decoder = BSH.ByteStreamHandler()
 
+
+
     def send_int_array(self,data_to_send):
         msg2send = self.byte_encoder.compose_byte_frame(data_to_send)
-        self.client_socket.sendto(msg2send, )
-
+        #self.client_socket.sendto(msg2send, )
+        pass
 
 if __name__ == "__main__":
     
    
     #(yl.dictionary['server']['ip'],yl.dictionary['server']['port'])
+    puppet = Client(yl.dictionary['client']['port'])
+    puppet.subscribe(server_ip= yl.dictionary['server']['ip'], server_port = yl.dictionary['server']['port'])
     print("sent")
     
 
--- python_impl/server.py
+++ python_impl/server.py
@@ -5,29 +5,38 @@ import byteStreamHandler as BSH
 
 class Server:
     subscriber_addresses = []
+    server_ok = 200
     def __init__(self,server_ip="",server_port=8080):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_info = (server_ip,server_port)
-        self.server_socket.bind('',self.server_info)
+        self.server_socket.bind(self.server_info)
+
     
     def get_subscribers(self):
         port, address = self.server_socket.recvfrom(1024)
-        print(port)
-        self.subscriber_addresses.append((address[0],int.from_bytes(port)))
-
-        self.server_socket.send(bytes('ok'),self.subscriber_addresses[0])
-
+        #print(port)
+        #print(int.from_bytes(port,'little'));
+        #print(address)
+        self.subscriber_addresses.append((address[0],int.from_bytes(port,'little')))
+        print(self.subscriber_addresses[0])
+        #self.server_socket.sendto(bytes(512),('127.0.0.1',8080))
+        self.server_socket.sendto(self.server_ok.to_bytes(1,'little'),(self.subscriber_addresses[0][0],
+                                                                    self.subscriber_addresses[0][1]))
+    def publish_int_array():
 
 
 if __name__ == "__main__":
 
-    ('', yl.dictionary['server']['port']))
-    byte_encoder = BSH.ByteStreamHandler()
+   # ('', yl.dictionary['server']['port']))
+    controller = Server(yl.dictionary['server']['ip'],yl.dictionary['server']['port'])
+    controller.get_subscribers()
 
-    data_to_send = [0,134,247,39,40,52]
+   # byte_encoder = BSH.ByteStreamHandler()
+    
+#data_to_send = [0,134,247,39,40,52]
 
-    msg = byte_decoder.decompose_byte_frame(message)
-    print((msg,address))
+    #msg = byte_decoder.decompose_byte_frame(message)
+    #print((msg,address))
 
 
 
