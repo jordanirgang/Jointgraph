ce2adf9b2bdf3a96d8d8551a147a697458edeee2
Jordan Irgang <jordanirgang@gmail.com>
2025-03-22 00:57:22 -0400

    proper angle encode and decode into blender with example

--- python_impl/blender_test.py
+++ python_impl/blender_test.py
@@ -2,16 +2,19 @@
 import bpy
 from bpy.app.handlers import persistent
 import sys 
-sys.path.insert(0,"/home/duckpcc/dev/code/Jointgraph/python_impl")
-sys.path.insert(0,"/home/duckpcc/dev/code/Jointgraph/resources")
+sys.path.insert(0,"/home/ducktop/code/cpp/Jointgraph/python_impl")
+sys.path.insert(0,"/home/ducktop/code/cpp/Jointgraph/resources")
 
 from joint_blender_impl import BlenderAngleImpl
 
+is_view_animation = False
+
+
 test_blender_angle = BlenderAngleImpl("Bone")
+test_blender_angle.is_animate = True
 hardcode= 0
 
 #https://docs.blender.org/api/current/bpy.app.handlers.html
-#TODO: add support for event trigering udp animations
 
 
 @persistent
@@ -20,12 +23,19 @@ def frame_pre_handler_udp_write(dummy):
 
 @persistent
 def frame_pre_handler_udp_listen(dummy):
-    hardcode += 1
+    hardcode = 1
     test_blender_angle.populate(hardcode)
-
-
-
+    
+def unregister_all_handlers():
+    while(len(bpy.app.handlers.frame_change_pre) >0):
+        bpy.app.handlers.frame_change_pre.pop(0)
+    
 
 if __name__ == "__main__":
-    print("start")
-    bpy.app.handlers.frame_change_pre.append(frame_pre_handler_udp_listen)
+    #print("start")
+    if not is_view_animation:
+        unregister_all_handlers()
+        bpy.app.handlers.frame_change_pre.append(frame_pre_handler_udp_listen)
+        print(len(bpy.app.handlers.frame_change_pre))
+    else:
+        unregister_all_handlers()
--- python_impl/joint_blender_impl.py
+++ python_impl/joint_blender_impl.py
@@ -1,5 +1,6 @@
 import bpy
 import math
+from mathutils import Euler
 from A_joint_impl import AJointImpl
 
 
@@ -11,19 +12,26 @@ class BlenderAngleImpl(AJointImpl):
 		self.stream_axis = stream_axis
 		self.frame = 0
 		self.data_path = ""
-		self.axis_dict ={'X':0,'Y':0,'Z':1}
+		self.axis_dict ={'X':0,'Y':1,'Z':2}
 		self.is_animate = False
-
+		self.data_path = "rotation_euler"
+		self.joint_angle_axis = [0,0,0]
+		self.joint_angle_axis[self.axis_dict[self.stream_axis]] = 1
+		print(self.joint_angle_axis)
 
 	def gather(self):
-		return math.degrees(self.pose_bone.rotation_euler[self.axis_dict[self.stream_axis]])
+		print(self.axis_dict)
+		return self.pose_bone.rotation_euler[self.axis_dict[self.stream_axis]]
 
 	def populate(self,joint_angle):
-		print((self.stream_axis,math.radians(joint_angle)))
-		self.pose_bone.rotation_euler.rotate_axis(self.stream_axis, math.radians(joint_angle))
+		self.joint_angle_axis[self.axis_dict[self.stream_axis]] = joint_angle
+		print(("tup",self.joint_angle_axis))
+		self.pose_bone.rotation_euler= Euler(self.joint_angle_axis,'XYZ')#.rotate_axis(self.stream_axis, math.radians(joint_angle))
+		print(self.pose_bone)
 		if self.is_animate:
-			self.pose_bone.keyframe_insert(self.data_path, frame=self.frame)	
+			self.pose_bone.keyframe_insert(data_path=self.data_path, frame=self.frame)	
 			self.frame +=1
+			#nothing
 
 
 
